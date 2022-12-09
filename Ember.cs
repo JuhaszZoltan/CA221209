123f5f22fcd6ac6651ddf1d4e244c78658abad20
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2022-12-09 15:16:14 +0100

    Add project files.

---
+++ Ember.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA221209
+{
+    internal class Ember
+    {
+        private string nev;
+        public string Nev
+        {
+            get => nev;
+            set
+            {
+                if (value.Trim().Length < 1)
+                    throw new Exception("minimu egy karakter hosszú kell, hogy legyen");
+                nev = value;
+            }
+        }
+
+        private (int x, int y) hely;
+        public (int X, int Y) Hely
+        {
+            set
+            {
+                if (value.X < 0 || value.X >= Console.WindowWidth)
+                    throw new Exception($"ez az érték csak [0; {Console.WindowWidth - 1}] között lehet!");
+                if (value.Y < 0 || value.Y >= Console.WindowHeight)
+                    throw new Exception($"ez az érték csak [0; {Console.WindowHeight - 1}] között lehet!");
+                hely = value;
+            }
+            get => hely;
+        }
+
+        public ConsoleColor szin;
+        
+
+        public void Megjelenit()
+        {
+            Console.ForegroundColor = szin;
+
+            Console.SetCursorPosition(
+                left: hely.x,
+                top: hely.y);
+            Console.Write(Nev[0]);
+
+            Console.ResetColor();
+            Console.SetCursorPosition(0, 0);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CA221209
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.SetWindowSize(20, 10);
+
+            Ember e1 = new();
+            e1.Hely = (X: 12, Y: 3);
+            e1.szin = ConsoleColor.Green;
+            e1.Nev = "Béla";
+
+            Ember e2 = new();
+            e2.Hely = (X: 2, Y: 7);
+            e2.szin = ConsoleColor.Blue;
+            e2.Nev = "M";
+
+            List<Ember> emberek = new() { e1, e2 };
+
+            e1.Nev = "Ágnes";
+            emberek[0].szin = ConsoleColor.Magenta;
+
+            Console.WriteLine(e1.Hely.X);
+
+
+            foreach (var e in emberek)
+            {
+                e.Megjelenit();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
